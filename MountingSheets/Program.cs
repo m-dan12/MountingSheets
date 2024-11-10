e7c616a3fd11f021c69d6e498806915fc457f446
Даниил Малинин <m-dan12@ya.ru>
2024-11-10 22:01:17 +0400

    Добавьте файлы проекта.

---
+++ MountingSheets/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MountingSheets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
